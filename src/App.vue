f118ba60b0ea8e5fff95b13877e7b1aa9160487e
gabrielpaoli <gabrielpaoli@hotmail.com>
2021-10-18 21:38:55 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Inicio</router-link> |
+      <router-link to="/buscar-profesional">Buscar profesional</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view />
   </div>
