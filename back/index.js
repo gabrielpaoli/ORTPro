ff6d7383455d567dc527d0370fcb18b832ccde0d
brianduffau <brianduffau@gmail.com>
2021-10-30 18:55:27 -0300

    add Profesionales en JSON

--- back/index.js
+++ back/index.js
@@ -66,8 +66,8 @@ app.get('/api/v1/mapData', function (req, res) {
               geometry: {
                 type: "Point",
                 coordinates: [
-                  -38.3613558,
-                  -8.8044875
+                  -58.4528200492791,
+								  -34.5959886570639
                 ]
               },
               properties: {
@@ -83,8 +83,8 @@ app.get('/api/v1/mapData', function (req, res) {
               geometry: {
                 type: "Point",
                 coordinates: [
-                  -36.3613558,
-                  -8.8044875
+                  -58.4604133778842,
+								  -34.6567978457031
                 ]
               },
               properties: {
@@ -100,8 +100,8 @@ app.get('/api/v1/mapData', function (req, res) {
               geometry: {
                 type: "Point",
                 coordinates: [
-                  -34.3613558,
-                  -8.8044875
+                  -58.4605276992685,
+                  -34.5782946601007
                 ]
               },
               properties: {
@@ -111,6 +111,125 @@ app.get('/api/v1/mapData', function (req, res) {
                 link: '/perfil/',
                 imageUrl: 'https://picsum.photos/200??',
               }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.4693741540716,
+								-34.6638256558981
+              ]
+            },
+            properties: {
+              id: 3,
+              nombre: "Jorge",
+              profesion: "Pintor",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.4578298572665,
+								-34.5970394940223
+              ]
+            },
+            properties: {
+              id: 4,
+              nombre: "Nestor",
+              profesion: "Albañil",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.4839883919569,
+								-34.5893724656529
+              ]
+            },
+            properties: {
+              id: 5,
+              nombre: "Cesar",
+              profesion: "Carpintero",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.4302546049882,
+								-34.6117598449431
+              ]
+            },
+            properties: {
+              id: 6,
+              nombre: "Jose",
+              profesion: "Herrero",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.5101486029498,
+								-34.6149979086219
+              ]
+            },
+            properties: {
+              id: 7,
+              nombre: "Marta",
+              profesion: "Costurera",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.4625305706803,
+								-34.6550596493391
+              ]
+            },
+            properties: {
+              id: 8,
+              nombre: "Mariano",
+              profesion: "Pintor",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [
+                -58.3803242076163,
+								-34.6574970602495
+              ]
+            },
+            properties: {
+              id: 9,
+              nombre: "Maria",
+              profesion: "Cocinera",
+              link: '/perfil/',
+              imageUrl: 'https://picsum.photos/200??',
+            }
           }
       ]
   
