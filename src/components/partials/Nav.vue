eed3e5035e83166de63199f28e7587ae870437b8
gabrielpaoli <gabrielpaoli@hotmail.com>
2021-11-07 18:11:18 -0300

    agregamos la integracion loca loca

---
+++ src/components/partials/Nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar container" role="navigation" aria-label="main navigation">
+    <div id="navbar" class="navbar-menu">
+      <div class="navbar-item mx-auto">
+        <div class="buttons">
+          <div v-if="!$auth.loading">
+            <a
+              v-if="!$auth.isAuthenticated"
+              @click="login"
+              class="button is-dark"
+              ><strong>Sign in</strong></a
+            >
+            <button v-on:click="showConsoleLog()">sasa</button>
+            <a
+              v-if="$auth.isAuthenticated"
+              @click="logout"
+              class="button is-dark"
+              ><strong>Log out</strong></a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Nav",
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
+    showConsoleLog() {
+      console.log(this.$auth);
+      console.log(this.$auth.user);
+    },
+  },
+};
+</script>
